653728aa9cf2223ff8b9870b61d29a11226ebc00
Alan Morales <alanrmorales@gmail.com>
2013-12-18 09:10:17 -0600

    Created the code for multithreaded

--- src/edu/macalester/moviedatabase/Main.java
+++ src/edu/macalester/moviedatabase/Main.java
@@ -1,8 +1,10 @@
 package edu.macalester.moviedatabase;
 
 
+import java.awt.List;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 
@@ -18,29 +20,53 @@ public class Main {
 		
 		database.intializeMovieTags("ml-10M100K/tags.dat");
 		
-		CollaborativeMutualInformation similarityMeasure = new CollaborativeMutualInformation(database);
+		final CollaborativeMutualInformation similarityMeasure = new CollaborativeMutualInformation(database);
 		
-		FileWriter writer = null;
-		
+		FileWriter fWriter = null;
 		try {
-			writer = new FileWriter("tags.csv");
+			fWriter = new FileWriter("tags.csv");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		writer.append('"' + " Tag 1 " + '"'+ ',' + '"' + " Tag 2 " + '"' + " , " + "Similarity");
-		writer.append('\n');
+		fWriter.append('"' + " Tag 1 " + '"'+ ',' + '"' + " Tag 2 " + '"' + " , " + "Similarity");
+		fWriter.append('\n');
 		
-		LinkedList<String> tags = new LinkedList<String>(database.getTagsSet());
+		final LinkedList<String> tags = new LinkedList<String>(database.getTagsSet());
 						
 		int threads  = 12;
 		
+		HashSet<TagCalculationProcedure> procedures = new HashSet<TagCalculationProcedure>();
+		final FileWriter writer = fWriter;
+		ParallelForEach.loop(tags,
+				threads,
+				new Procedure<String>() {
+                    @Override
+                    public void call(String comparingTag) throws Exception {
+                    	int start = tags.indexOf(comparingTag);
+                    	for(String comparedTag : tags.subList(start, tags.size() )){
+							
+            				double cc = similarityMeasure.calculateSimilarity(comparingTag, comparedTag);
+            				
+            				if(cc != 0){
+            					
+            					// Remove newlines, commas and apostrophes that may distort the CSV file when being written.
+            					synchronized(writer){
+            					writer.append('"' + comparingTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"'+ ',' + '"' + comparedTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"' + " , " + cc);
+            					writer.append('\n');
+            					}
+            				}
+            				
+            			}
+                    }
+                }
+				);
 		for(int i = 0; i < threads; i++){
-			new TagCalculationProcedure<>(similarityMeasure, tags, tags.size()/threads * i, tags.size()/threads * (i+1), writer);
+			procedures.add(new TagCalculationProcedure<>(similarityMeasure, tags, tags.size()/threads * i, tags.size()/threads * (i+1), fWriter));
 		}
 				
-		writer.flush();
-	    writer.close();
+		fWriter.flush();
+	    fWriter.close();
 		
 	}
 
