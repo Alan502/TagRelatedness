9efcc1d06d6891bc871f725b52f56b8e747b519e
Alan Morales <alanrmorales@gmail.com>
2013-12-23 16:28:02 -0600

    put everything in methods in the main class

--- src/edu/macalester/moviedatabase/Database.java
+++ src/edu/macalester/moviedatabase/Database.java
@@ -1,8 +1,11 @@
 package edu.macalester.moviedatabase;
 
+import java.util.LinkedList;
+
 public interface Database {
 	
 	public void initializeMovieLensTags(String dir);
 	public void initializeBibSonomyTags(String dir);
+	public LinkedList<String> getTagsSet();
 
 }
--- src/edu/macalester/moviedatabase/Main.java
+++ src/edu/macalester/moviedatabase/Main.java
@@ -1,5 +1,10 @@
 package edu.macalester.moviedatabase;
 
+import java.awt.BufferCapabilities;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -12,39 +17,59 @@ import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
-						
+		tauBetweenCSVandWordnet("distributional_matching.csv");		
+	}
+	
+	public static void tauBetweenCSVandWordnet(String file){
+		
+		WS4JConfiguration.getInstance().setMFS(true);
+        ILexicalDatabase db = new NictWordNet();
+		JiangConrath rc = new JiangConrath(db);
+		
+		ArrayList<Double> distMatchingSimilarities  = new ArrayList<Double>();
+		ArrayList<Double> wordnetSimilarities = new ArrayList<Double>();
+		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = "";
+			br.readLine(); // Skip first line, which is just the column tags.
+			while ((line = br.readLine()) != null) {
+				 String[] column = line.split(",");
+				 double jc = rc.calcRelatednessOfWords(column[0].replace("\"", "") , column[1].replaceAll("\"", ""));
+				 if(jc != 0){
+					 distMatchingSimilarities.add(Double.parseDouble(column[2]));
+					 wordnetSimilarities.add(jc);
+				 }
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(distMatchingSimilarities.size()+" "+wordnetSimilarities.size());
+	    System.out.println(KendallsCorrelation.correlation(distMatchingSimilarities, wordnetSimilarities));
+	}
+	
+	public static void generateTagSimilarityCSV() throws IOException{
 		ProjectionalDatabase database = new ProjectionalDatabase();
 		database.intializeMovieLensTags("ml-10M100K/tags.dat");		
 		final DistributionalMatching similarityMeasure = new DistributionalMatching(database);
 		
-//		FileWriter fWriter = null;
-//		try {
-//			fWriter = new FileWriter("tags.csv");
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		fWriter.append('"' + " Tag 1 " + '"'+ ',' + '"' + " Tag 2 " + '"' + " , " + "Similarity");
-//		fWriter.append('\n');
+		FileWriter fWriter = null;
+		try {
+			fWriter = new FileWriter("tags.csv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		fWriter.append('"' + " Tag 1 " + '"'+ ',' + '"' + " Tag 2 " + '"' + " , " + "Similarity");
+		fWriter.append('\n');
 		
 		final LinkedList<String> tags = new LinkedList<String>(database.getTagsSet());
 						
-		int threads  = 12;		
+		int threads  = 12;
 		
-		// Initialize code necessary to calculate tau between this tag set and Wordnet
-		WS4JConfiguration.getInstance().setMFS(true);
-        ILexicalDatabase db = new NictWordNet();
-		final JiangConrath rc = new JiangConrath(db);
-		final ArrayList<Double> distMatchingSimilarities  = new ArrayList<Double>();
-		final ArrayList<Double> wordnetSimilarities = new ArrayList<Double>();
-		// End of code necessary for calculating tau
-		
-//		final FileWriter writer = fWriter;
+		final FileWriter writer = fWriter;
 		ParallelForEach.loop(tags,
 				threads,
 				new Procedure<String>() {
@@ -58,23 +83,10 @@ public class Main {
             				if(cc != 0){
             					
             					// Remove newlines, commas and apostrophes that may distort the CSV file when being written.
-//            					synchronized(writer){
-//            					writer.append('"' + comparingTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"'+ ',' + '"' + comparedTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"' + " , " + cc);
-//            					writer.append('\n');
-//            					}
-            					
-            					// Adding to arrays used to calculate TAU
-            					double jc = rc.calcRelatednessOfWords(comparedTag, comparingTag);
-            					if(jc != 0){
-            						
-            						synchronized (wordnetSimilarities) {
-            							wordnetSimilarities.add(jc);	
-            						}
-            						synchronized (distMatchingSimilarities) {
-            							distMatchingSimilarities.add(cc);
-									}
-            					}
-            						
+            					synchronized(writer){
+            					writer.append('"' + comparingTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"'+ ',' + '"' + comparedTag.replace('"', ' ').replace('\n', ' ').replace(',', ' ') + '"' + " , " + cc);
+            					writer.append('\n');
+            					}           						
             						
             						
             				}
@@ -84,12 +96,8 @@ public class Main {
                 }
 				);
 
-//		fWriter.flush();
-//	    fWriter.close();
-		
-		System.out.println(distMatchingSimilarities.size()+"  "+wordnetSimilarities.size());
-	    
-	    System.out.println(KendallsCorrelation.correlation(distMatchingSimilarities, wordnetSimilarities));
+		fWriter.flush();
+	    fWriter.close();
 		
 	}
 
