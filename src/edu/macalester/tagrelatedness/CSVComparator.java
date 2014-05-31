ec77a3eeb1a809c3a213ed17e0c2451addfe5cef
Alan Morales <alanrmorales@gmail.com>
2014-05-31 17:19:09 -0500

    dkfjd

--- src/edu/macalester/tagrelatedness/CSVComparator.java
+++ src/edu/macalester/tagrelatedness/CSVComparator.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class CSVComparator implements Comparator<String>{
 
 	public int compare(String r1, String r2) {
-		String[] data1 = r1.replace("\"", "").split(",");
-		String[] data2 = r2.replace("\"", "").split(",");
-		double result = 0;
+		String[] data1 = r1.split(",");
+		String[] data2 = r2.split(",");
+		double result;
 		
 		try{
 			result = Double.parseDouble(data1[2]) - Double.parseDouble(data2[2]);
